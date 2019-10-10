623f2286c2815c254389cd6c10269eb9020158b8
Your Name <you@example.com>
2019-10-10 08:41:47 +0800

    can send sms and test it

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,22 @@
+import json
+
 from django.test import TestCase
 
 # Create your tests here.
+data = {
+    'uid':1,
+    'name':'亚伦',
+    "age":23,
+    'gender':"男",
+    'result':{
+        'bool':False,
+    }
+}
+
+print(data,type(data))
+
+data1 = json.dumps(data)
+print(data1,type(data1))
+
+data2 = json.loads(data1)
+print(data2,type(data2))
